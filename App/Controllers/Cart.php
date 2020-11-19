202cc7614b5a66cf64e47df4800ccbc103cc4560
dduartee <gkduarte234@gmail.com>
2020-11-18 23:03:07 -0300

    diagrama exemplar para ser utilizado, funções comentadas...

--- App/Controllers/Cart.php
+++ App/Controllers/Cart.php
@@ -6,15 +6,21 @@ use App\Models\Cart as ModelsCart;
 
 class Cart {
 
+    /**
+     * Função principal, será chamada quando a url será cart/
+     *
+     * @return void
+     */
     public function index() {
         /**
          * faça a verificação se o usuario esta logado para receber o id dele
          */
+
         $userID = 1;
-        if(isset($_POST['id']) && isset($_POST['quantity']) && isset($_POST['add'])) { ### verifica se a função é para adicionar
+        
+        if(isset($_POST['id']) && isset($_POST['quantity']) && isset($_POST['add'])) {
             $this->addItem($userID, $_POST['id'], $_POST['quantity']);
         }
-
         $cart = new ModelsCart();
         $cartItems = $cart->getCartItems($userID);
 
@@ -23,6 +29,14 @@ class Cart {
          */
     }
 
+    /**
+     * Função que será chamada se o POST add existir
+     *
+     * @param integer $userID
+     * @param integer $id
+     * @param integer $quantity
+     * @return void
+     */
     private function addItem(int $userID, int $id, int $quantity) {
         $cart = new ModelsCart();
         $product = array($id, $quantity);
